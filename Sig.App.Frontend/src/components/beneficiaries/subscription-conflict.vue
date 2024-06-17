5b0e3c7c69f43f772afa5fe87526e02884504a8c
Jonathan Chartier <jonathan.chartier@sigmund.ca>
2024-06-17 14:46:01 -0400

    Adjust text after comment by Liam

--- Sig.App.Frontend/src/components/beneficiaries/subscription-conflict.vue
+++ Sig.App.Frontend/src/components/beneficiaries/subscription-conflict.vue
@@ -2,21 +2,17 @@
 {
   "en": {
     "select-all": "Select all",
-    "remaining-budget-allowance": "Remaining envelope: {amount}",
+    "remaining-budget-allowance": "→ Remaining envelope for this subscription: <b>{amount}</b>",
     "remaining-budget-allowance-after-adjustment": "Remaining envelope: {amount} after adjustment",
     "payment-to-receive": "→ <b>One</b> payment remaining | → <b>{count}</b> payments remaining",
-    "participant-miss-payment": "→ Participant will <b>not receive</b> this amount",
-    "participant-get-payment": "→ Participant will <b>receive</b> this amount",
-    "participant-category-detail": "→ Participant moves from category <b>{previousCategory} ({previousCategoryAmount})</b> to category <b>{newCategory} ({newCategoryAmount})</b>",
+    "participant-category-detail": "→ The participant will move from category <b>{previousCategory} ({previousCategoryAmount})</b> to category <b>{newCategory} ({newCategoryAmount})</b> for future payments",
 	},
 	"fr": {
 		"select-all": "Tout sélectionner",
-    "remaining-budget-allowance": "Enveloppe restante : {amount}",
-    "remaining-budget-allowance-after-adjustment": "Enveloppe restante : {amount} après l'ajustement",
+    "remaining-budget-allowance": "→ Enveloppe restante pour cet abonnement : <b>{amount}</b>",
+    "remaining-budget-allowance-after-adjustment": "→ Enveloppe restante pour cet abonnement : <b>{amount}</b> après l'ajustement",
     "payment-to-receive": "→ <b>Un</b> versement restant | → <b>{count}</b> versements restants",
-    "participant-miss-payment": "→ Le participant ne va <b>pas recevoir</b> ce montant",
-    "participant-get-payment": "→ Le participant va <b>recevoir</b> ce montant",
-    "participant-category-detail": "→ Le participant passe de la catégorie <b>{previousCategory} ({previousCategoryAmount})</b> à la catégorie <b>{newCategory} ({newCategoryAmount})</b>",
+    "participant-category-detail": "→ Le-a participant-e passera de la catégorie <b>{previousCategory} ({previousCategoryAmount})</b> à la catégorie <b>{newCategory} ({newCategoryAmount})</b> pour les versements futurs",
 	}
 }
 </i18n>
@@ -31,17 +27,21 @@
         :disabled="option.disabled"
         @input="(e) => updateCheckbox(option.value, e)">
         <template #description>
+          <!-- eslint-disable vue/no-v-html @intlify/vue-i18n/no-v-html -->
+          <p
+            class="mb-2 text-p2 leading-none"
+            :class="
+              isChecked(option.value)
+                ? option.previousPaymentAmount - option.newPaymentAmount > 0
+                  ? 'text-green-300'
+                  : option.previousPaymentAmount - option.newPaymentAmount < 0
+                  ? 'text-red-500'
+                  : ''
+                : ''
+            "
+            v-html="remainingBudgetAllowance(option)"></p>
           <!-- eslint-disable-next-line vue/no-v-html @intlify/vue-i18n/no-v-html -->
           <p class="mb-2 text-p2 leading-none" v-html="t('payment-to-receive', option.numberOfPaymentToReceive)"></p>
-          <!-- eslint-disable vue/no-v-html @intlify/vue-i18n/no-v-html -->
-          <p class="mb-2 text-p2 leading-none" v-html="option.description"></p>
-
-          <p
-            v-if="option.newPaymentAmount > option.previousPaymentAmount"
-            class="mb-2 text-p2 leading-none"
-            v-html="t('participant-get-payment')"></p>
-          <!-- eslint-disable-next-line vue/no-v-html @intlify/vue-i18n/no-v-html -->
-          <p v-else class="mb-2 text-p2 leading-none" v-html="t('participant-miss-payment')"></p>
           <!-- eslint-disable vue/no-v-html @intlify/vue-i18n/no-v-html -->
           <p
             class="mb-2 text-p2 leading-none"
@@ -53,31 +53,8 @@
                 newCategoryAmount: getMoneyFormat(option.newCategoryAmount)
               })
             "></p>
-          <b
-            class="pb-2 text-p2 leading-none"
-            :class="
-              isChecked(option.value)
-                ? option.previousPaymentAmount - option.newPaymentAmount > 0
-                  ? 'text-green-300'
-                  : option.previousPaymentAmount - option.newPaymentAmount < 0
-                  ? 'text-red-500'
-                  : ''
-                : ''
-            ">
-            <template v-if="isChecked(option.value)">
-              {{
-                t("remaining-budget-allowance-after-adjustment", {
-                  amount: getMoneyFormat(
-                    option.budgetAllowanceAvailableFund +
-                      (option.previousPaymentAmount - option.newPaymentAmount) * option.numberOfPaymentToReceive
-                  )
-                })
-              }}
-            </template>
-            <template v-else>
-              {{ t("remaining-budget-allowance", { amount: getMoneyFormat(option.budgetAllowanceAvailableFund) }) }}
-            </template>
-          </b>
+          <!-- eslint-disable vue/no-v-html @intlify/vue-i18n/no-v-html -->
+          <p class="mb-2 text-p2 leading-none" v-html="option.description"></p>
         </template>
       </PfFormInputCheckbox>
     </div>
@@ -119,6 +96,17 @@ const { t } = useI18n();
 const emit = defineEmits(["input", "checkAll"]);
 
 const isAllChecked = computed(() => props.value.length === props.options.length);
+const remainingBudgetAllowance = (option) => {
+  if (isChecked(option.value)) {
+    return t("remaining-budget-allowance-after-adjustment", {
+      amount: getMoneyFormat(
+        option.budgetAllowanceAvailableFund +
+          (option.previousPaymentAmount - option.newPaymentAmount) * option.numberOfPaymentToReceive
+      )
+    });
+  }
+  return t("remaining-budget-allowance", { amount: getMoneyFormat(option.budgetAllowanceAvailableFund) });
+};
 
 function isChecked(value) {
   return props.value.indexOf(value) !== -1;
