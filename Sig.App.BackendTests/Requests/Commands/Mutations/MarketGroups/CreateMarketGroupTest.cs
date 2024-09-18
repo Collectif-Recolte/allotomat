b0a0afd09fad70feaaf9aba951ce6ac5973ab79d
Jonathan Chartier <jonathan.chartier@sigmund.ca>
2024-09-18 10:40:26 -0400

    Fix CreateMarketGroup test since adding ProjectId in Mutation

--- Sig.App.BackendTests/Requests/Commands/Mutations/MarketGroups/CreateMarketGroupTest.cs
+++ Sig.App.BackendTests/Requests/Commands/Mutations/MarketGroups/CreateMarketGroupTest.cs
@@ -2,7 +2,9 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
+using Sig.App.Backend.DbModel.Entities.Projects;
 using Sig.App.Backend.EmailTemplates.Models;
+using Sig.App.Backend.Extensions;
 using Sig.App.Backend.Requests.Commands.Mutations.MarketGroups;
 using Sig.App.Backend.Services.Mailer;
 using System.Threading;
@@ -14,11 +16,21 @@ namespace Sig.App.BackendTests.Requests.Commands.Mutations.MarketGroups
     public class CreateMarketGroupTest : TestBase
     {
         private readonly CreateMarketGroup handler;
+        private readonly Project project;
         private Mock<IMailer> mailer;
 
         public CreateMarketGroupTest()
         {
             mailer = new Mock<IMailer>();
+
+            project = new Project()
+            {
+                Name = "Project 1"
+            };
+            DbContext.Projects.Add(project);
+
+            DbContext.SaveChanges();
+
             handler = new CreateMarketGroup(NullLogger<CreateMarketGroup>.Instance, DbContext, UserManager, mailer.Object);
         }
 
@@ -28,7 +40,8 @@ namespace Sig.App.BackendTests.Requests.Commands.Mutations.MarketGroups
             var input = new CreateMarketGroup.Input()
             {
                 Name = "MarketGroup Test 1",
-                ManagerEmails = new string[1] { "test1@example.com" }
+                ManagerEmails = new string[1] { "test1@example.com" },
+                ProjectId = project.GetIdentifier()
             };
 
             await handler.Handle(input, CancellationToken.None);
@@ -44,7 +57,8 @@ namespace Sig.App.BackendTests.Requests.Commands.Mutations.MarketGroups
             var input = new CreateMarketGroup.Input()
             {
                 Name = "MarketGroup Test 1",
-                ManagerEmails = new string[1] { "test1@example.com" }
+                ManagerEmails = new string[1] { "test1@example.com" },
+                ProjectId = project.GetIdentifier()
             };
 
             await handler.Handle(input, CancellationToken.None);
