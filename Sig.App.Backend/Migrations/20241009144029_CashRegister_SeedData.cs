72d72bb644f059697e334b6bd5c2b7d2b464a69f
Jonathan Chartier <jonathan.chartier@sigmund.ca>
2025-01-28 16:12:24 -0500

    Adjust Default market group and Default cash register name to be billingual

--- Sig.App.Backend/Migrations/20241009144029_CashRegister_SeedData.cs
+++ Sig.App.Backend/Migrations/20241009144029_CashRegister_SeedData.cs
@@ -10,13 +10,13 @@ namespace Sig.App.Backend.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("INSERT INTO MarketGroups (ProjectId, Name, IsArchived) SELECT p.Id, 'Groupe de commerce par défaut', 0 FROM Projects p WHERE p.Id NOT IN (SELECT mg.ProjectId FROM MarketGroups mg)");
+            migrationBuilder.Sql("INSERT INTO MarketGroups (ProjectId, Name, IsArchived) SELECT p.Id, 'Groupe de commerce par défaut / Default market group', 0 FROM Projects p WHERE p.Id NOT IN (SELECT mg.ProjectId FROM MarketGroups mg)");
 
             migrationBuilder.Sql("INSERT INTO MarketGroupMarkets (MarketGroupId, MarketId) SELECT mg.Id AS MarketGroupId, pm.MarketId FROM ProjectMarkets pm JOIN MarketGroups mg ON mg.ProjectId = pm.ProjectId");
 
-            migrationBuilder.Sql("INSERT INTO CashRegisters (Name, MarketId, IsArchived) SELECT 'Caisse par défaut', m.Id, 0 FROM Markets m WHERE m.Id NOT IN (SELECT cr.MarketId FROM CashRegisters cr)");
+            migrationBuilder.Sql("INSERT INTO CashRegisters (Name, MarketId, IsArchived) SELECT 'Caisse par défaut / Default cash register', m.Id, 0 FROM Markets m WHERE m.Id NOT IN (SELECT cr.MarketId FROM CashRegisters cr)");
 
-            migrationBuilder.Sql("INSERT INTO CashRegisterMarketGroups (MarketGroupId, CashRegisterId) SELECT mg.Id AS MarketGroupId, cr.Id AS CashRegisterId FROM MarketGroupMarkets mgm JOIN CashRegisters cr ON cr.MarketId = mgm.MarketId JOIN MarketGroups mg ON mg.Id = mgm.MarketGroupId WHERE cr.Name = 'Caisse par défaut'");
+            migrationBuilder.Sql("INSERT INTO CashRegisterMarketGroups (MarketGroupId, CashRegisterId) SELECT mg.Id AS MarketGroupId, cr.Id AS CashRegisterId FROM MarketGroupMarkets mgm JOIN CashRegisters cr ON cr.MarketId = mgm.MarketId JOIN MarketGroups mg ON mg.Id = mgm.MarketGroupId WHERE cr.Name = 'Caisse par défaut / Default cash register'");
 
             migrationBuilder.Sql("UPDATE tl SET tl.CashRegisterId = cr.Id, tl.CashRegisterName = cr.Name FROM TransactionLogs tl JOIN CashRegisters cr ON tl.MarketId = cr.MarketId");
 
