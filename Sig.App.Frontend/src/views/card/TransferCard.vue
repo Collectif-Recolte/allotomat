9b0c4cce5b53eb4fb744ae5c6f325122e1e60ef9
C. Liam Brown <138715805+liam-recolte@users.noreply.github.com>
2026-03-16 14:25:09 -0400

    Update TransferCard.vue

--- Sig.App.Frontend/src/views/card/TransferCard.vue
+++ Sig.App.Frontend/src/views/card/TransferCard.vue
@@ -28,9 +28,9 @@
       "confirm-desc": "La carte #{cardNumber} est maintenant active et prête à l'emploi.",
       "close": "Fermer",
       "original-card-not-found": "L'ID de la carte perdue n'existe pas.",
-      "original-card-not-assign": "La carte perdue n'est pas assignée à un-e participant-e.",
+      "original-card-not-assign": "La carte perdue n'est pas assignée à un·e participant·e.",
       "new-card-not-found": "L'ID de la nouvelle carte n'existe pas.",
-      "new-card-already-assign": "La carte que vous essayez d'assigner est déjà assignée à un-e autre participant-e.",
+      "new-card-already-assign": "La carte que vous essayez d'assigner est déjà assignée à un·e autre participant·e.",
       "new-card-already-lost": "La carte que vous essayez d'assigner est déjà perdue ou endommagée.",
       "new-card-already-gift-card": "La carte que vous essayez d'assigner est une carte-cadeau."
     }
