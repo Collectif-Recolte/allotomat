84fbb8bd92d457009a1e40dbd19c8e2216ef6b0c
C. Liam Brown <138715805+liam-recolte@users.noreply.github.com>
2026-03-16 13:16:07 -0400

    Update RemoveMarketFromOrganization.vue

--- Sig.App.Frontend/src/views/organization/RemoveMarketFromOrganization.vue
+++ Sig.App.Frontend/src/views/organization/RemoveMarketFromOrganization.vue
@@ -2,14 +2,14 @@
 {
 	"en": {
 		"delete-market-success-notification": "The merchant {marketName} has been successfully removed from the group.",
-		"delete-text-error": "The text must match the name of the merchant",
+		"delete-text-error": "The text must match the name of the merchant.",
 		"delete-text-label": "Type the name of the merchant to confirm",
 		"description": "Warning! If you continue, <strong>{marketName}</strong> will be removed from the group permanently.",
 		"title": "Remove - {marketName}"
 	},
 	"fr": {
 		"delete-market-success-notification": "Le commerce {marketName} a été retiré du groupe avec succès.",
-		"delete-text-error": "Le texte doit correspondre au nom du commerce",
+		"delete-text-error": "Le texte doit correspondre au nom du commerce.",
 		"delete-text-label": "Taper le nom du commerce pour confirmer",
 		"description": "Avertissement ! Si vous continuez, <strong>{marketName}</strong> sera retiré du groupe de façon.",
 		"title": "Retirer - {marketName}"
