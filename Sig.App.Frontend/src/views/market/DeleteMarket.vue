cd0abcd6d147056a8e7bd1d4ace6eb14f316ebb7
C. Liam Brown <138715805+liam-recolte@users.noreply.github.com>
2026-03-16 09:06:10 -0400

    Update DeleteMarket.vue

--- Sig.App.Frontend/src/views/market/DeleteMarket.vue
+++ Sig.App.Frontend/src/views/market/DeleteMarket.vue
@@ -1,17 +1,17 @@
 <i18n>
 {
 	"en": {
-		"delete-market-success-notification": "The market {marketName} has been successfully deleted.",
-		"delete-text-error": "The text must match the name of the market",
-		"delete-text-label": "Type the name of the market to confirm",
-		"description": "Warning ! The deletion of <strong>{marketName}</strong> cannot be undone. If you continue, the market will be permanently deleted along with all the elements it contains. Card payments can no longer be traced. However, the funds of the cards will remain unchanged.",
+		"delete-market-success-notification": "The merchant {marketName} has been successfully deleted.",
+		"delete-text-error": "The text must match the name of the merchant",
+		"delete-text-label": "Type the name of the merchant to confirm",
+		"description": "Warning! Deleting <strong>{marketName}</strong> cannot be undone. If you continue, the merchant will be permanently deleted along with all the elements it contains. No card balances will be affected, but transactions for this merchant will no longer be available.",
 		"title": "Delete - {marketName}"
 	},
 	"fr": {
 		"delete-market-success-notification": "Le commerce {marketName} a été supprimé avec succès.",
 		"delete-text-error": "Le texte doit correspondre au nom du commerce",
 		"delete-text-label": "Taper le nom du commerce pour confirmer",
-		"description": "Avertissement ! La suppression de <strong>{marketName}</strong> ne peut pas être annulée. Si vous continuez, le commerce sera supprimé ainsi que tous les éléments qu'il contient de façon définitive. Les paiements sur les cartes ne pourront plus être retracés. Par contre, les fonds des cartes vont rester inchangés.",
+		"description": "Avertissement ! La suppression de <strong>{marketName}</strong> ne peut pas être annulée. Si vous continuez, le commerce sera supprimé ainsi que tous les éléments qu'il contient de façon définitive. Les soldes des cartes ne seront pas affectés, mais les transactions auprès de ce commerce ne seront plus disponibles.",
 		"title": "Supprimer - {marketName}"
 	}
 }
