343c996e772111f57a3b9ddffbc2645c9752b2dc
Jonathan Chartier <jonathan.chartier@sigmund.ca>
2026-03-24 15:39:51 -0400

    Improve cash-register name (remove default cash-register name) | Add migration to change the cash-register name to the new default

--- Sig.App.Frontend/src/views/transaction/NewScan.vue
+++ Sig.App.Frontend/src/views/transaction/NewScan.vue
@@ -25,16 +25,10 @@
 
 <template>
   <div class="flex justify-center h-full w-full py-8 lg:py-16">
-    <UiCta
-      v-if="selectedCashRegisterId !== null && market !== null"
-      class="w-full max-w-sm"
-      :img-src="require('@/assets/img/scan-marchand.jpg')"
-      :primary-btn-label="t('start-transaction')"
-      :secondary-btn-label="t('manually-enter-card-number')"
-      :primary-btn-disabled="market.isDisabled"
-      :secondary-btn-disabled="market.isDisabled"
-      primary-btn-is-action
-      secondary-btn-is-action
+    <UiCta v-if="selectedCashRegisterId !== null && market !== null" class="w-full max-w-sm"
+      :img-src="require('@/assets/img/scan-marchand.jpg')" :primary-btn-label="t('start-transaction')"
+      :secondary-btn-label="t('manually-enter-card-number')" :primary-btn-disabled="market.isDisabled"
+      :secondary-btn-disabled="market.isDisabled" primary-btn-is-action secondary-btn-is-action
       @onPrimaryBtnClick="emit('onUpdateStep', TRANSACTION_STEPS_SCAN, {})"
       @onSecondaryBtnClick="emit('onUpdateStep', TRANSACTION_STEPS_MANUALLY_ENTER_CARD_NUMBER, {})">
       <div v-if="!loading" class="mb-6 text-left relative border border-primary-300 rounded-lg p-4 w-full">
@@ -42,17 +36,13 @@
           <h3 class="text-h4 font-semibold text-primary-900 mt-2 mb-2">
             <span>{{ selectedCashRegister.name }}</span>
           </h3>
-          <PfButtonAction
-            :disabled="cashRegisters.length === 1"
-            is-icon-only
-            btn-style="outline"
-            :icon="ICON_PENCIL"
+          <PfButtonAction :disabled="cashRegisters.length === 1" is-icon-only btn-style="outline" :icon="ICON_PENCIL"
             @click="editCashRegister" />
         </div>
         <ul class="mb-0">
           <li v-for="marketGroup in selectedCashRegister.marketGroups" :key="marketGroup.id" class="text-p2">
             <div>{{ marketGroup.project.name }}</div>
-            <div>{{ marketGroup.name }}</div>
+            <div v-if="marketGroup.name !== selectedCashRegister.name">{{ marketGroup.name }}</div>
           </li>
         </ul>
       </div>
@@ -68,18 +58,15 @@
             <PfForm has-footer :disable-submit="Object.keys(formErrors).length > 0">
               <PfFormSection>
                 <Field v-slot="{ field, errors: fieldErrors }" name="selectedCashRegister">
-                  <PfFormInputSelect
-                    id="selectedCashRegister"
-                    v-bind="field"
-                    :label="t('cash-register-input')"
-                    :options="cashRegisterOptions"
-                    :errors="fieldErrors" />
+                  <PfFormInputSelect id="selectedCashRegister" v-bind="field" :label="t('cash-register-input')"
+                    :options="cashRegisterOptions" :errors="fieldErrors" />
                 </Field>
               </PfFormSection>
               <template #footer>
                 <div class="pt-5">
                   <div class="flex w-full justify-end">
-                    <PfButtonAction class="px-8" btn-style="secondary" :label="t('select-cash-register')" type="submit" />
+                    <PfButtonAction class="px-8" btn-style="secondary" :label="t('select-cash-register')"
+                      type="submit" />
                   </div>
                 </div>
               </template>
