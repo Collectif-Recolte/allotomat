371bdc8f7d11b7c7ad100b1fe420b5bff5b9b699
Jonathan Chartier <jonathan.chartier@sigmund.ca>
2026-04-06 17:36:22 -0400

    Change text for First/Last in french

--- Sig.App.Frontend/src/components/ui/pagination.vue
+++ Sig.App.Frontend/src/components/ui/pagination.vue
@@ -7,8 +7,8 @@
 		"previous": "Previous"
 	},
 	"fr": {
-		"first": "Premier",
-		"last": "Dernier",
+		"first": "Première",
+		"last": "Dernière",
 		"next": "Suivant",
 		"previous": "Précédent"
 	}
@@ -18,15 +18,13 @@
 <template>
   <nav class="border-t border-grey-200 dark:border-grey-800 px-4 flex items-center justify-between sm:px-0">
     <div class="-mt-px flex gap-x-1">
-      <button
-        v-if="props.page > 1"
+      <button v-if="props.page > 1"
         class="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-semibold text-grey-500 hover:text-grey-700 hover:border-grey-300 dark:text-grey-400 dark:hover:text-grey-300 dark:hover:border-grey-700"
         @click="onPageClick(1)">
         <PfIcon class="shrink-0 mr-1" size="sm" :icon="CHEVRON_DOUBLE_LEFT_ICON" aria-hidden="true" />
         {{ t("first") }}
       </button>
-      <button
-        v-if="props.page > 1"
+      <button v-if="props.page > 1"
         class="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-semibold text-grey-500 hover:text-grey-700 hover:border-grey-300 dark:text-grey-400 dark:hover:text-grey-300 dark:hover:border-grey-700"
         @click="onPageClick(props.page - 1)">
         <PfIcon class="shrink-0 mr-1" size="sm" :icon="CHEVRON_LEFT_ICON" aria-hidden="true" />
@@ -36,19 +34,15 @@
 
     <div class="hidden md:-mt-px md:flex">
       <template v-for="(pageItem, index) in paginationItems" :key="index">
-        <span
-          v-if="pageItem === '...'"
-          class="border-transparent text-grey-500 dark:text-grey-400 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-semibold"
-          >{{ pageItem }}</span
-        >
-        <span
-          v-else-if="parseInt(pageItem) === props.page"
+        <span v-if="pageItem === '...'"
+          class="border-transparent text-grey-500 dark:text-grey-400 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-semibold">{{
+          pageItem }}</span>
+        <span v-else-if="parseInt(pageItem) === props.page"
           class="border-primary-500 text-primary-700 dark:text-primary-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-semibold"
           :aria-current="pageItem">
           {{ pageItem }}
         </span>
-        <button
-          v-else
+        <button v-else
           class="border-transparent text-grey-500 hover:text-grey-700 hover:border-grey-300 dark:text-grey-400 dark:hover:text-grey-300 dark:hover:border-grey-700 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-semibold"
           @click="onPageClick(parseInt(pageItem))">
           {{ pageItem }}
@@ -57,15 +51,13 @@
     </div>
 
     <div class="-mt-px flex gap-x-1 justify-end">
-      <button
-        v-if="props.page < props.totalPages"
+      <button v-if="props.page < props.totalPages"
         class="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-semibold text-grey-500 hover:text-grey-700 hover:border-grey-300 dark:text-grey-400 dark:hover:text-grey-300 dark:hover:border-grey-700"
         @click="onPageClick(props.page + 1)">
         {{ t("next") }}
         <PfIcon class="shrink-0 ml-1" size="sm" :icon="CHEVRON_RIGHT_ICON" aria-hidden="true" />
       </button>
-      <button
-        v-if="props.page < props.totalPages"
+      <button v-if="props.page < props.totalPages"
         class="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-semibold text-grey-500 hover:text-grey-700 hover:border-grey-300 dark:text-grey-400 dark:hover:text-grey-300 dark:hover:border-grey-700"
         @click="onPageClick(props.totalPages)">
         {{ t("last") }}
