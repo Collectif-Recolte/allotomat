ad4edc629e5c33781fc0aea9b299c7f346fc8f93
Mathieu Deslauriers <mathieu.deslauriers@sigmund.ca>
2026-06-17 14:01:12 -0400

    Refactorer select-searchable avec Combobox Headless UI et filtrage normalisé.

---
+++ Sig.App.Frontend/pinkflamant/components/form/input/combobox-open-watcher.vue
@@ -0,0 +1,25 @@
+<script>
+import { defineComponent, watch } from "vue";
+
+export default defineComponent({
+  name: "ComboboxOpenWatcher",
+  props: {
+    open: {
+      type: Boolean,
+      required: true
+    }
+  },
+  emits: ["close"],
+  setup(props, { emit }) {
+    watch(
+      () => props.open,
+      (open, wasOpen) => {
+        if (wasOpen && !open) {
+          emit("close");
+        }
+      }
+    );
+    return () => null;
+  }
+});
+</script>
